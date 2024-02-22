6b72d5a766e4b68fe75c2ac16919c17293504a32
Taha Kaan Yılmaz <142171450+kaanylmz34@users.noreply.github.com>
2024-02-22 10:20:00 +0000

    step 4

--- comments.js
+++ comments.js
@@ -1,55 +1,35 @@
 // Create Web Server
-// Simple and easy to use
-const express = require('express');
-const app = express();
-const port = 3000;
 
-// Set view engine
-app.set('view engine', 'pug');
+// Required modules
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var comments = [];
 
-// Set static file
-app.use('/static', express.static('public'))
-
-// Set route
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-// Set route
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-// Set route
-app.get('/project', (req, res) => {
-  res.render('project');
-});
-
-// Set route
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-// Set route
-app.get('/error', (req, res) => {
-  res.render('error');
-});
-
-// Set route
-app.use((req, res, next) => {
-  const err = new Error('Page Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Set error route
-app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render('error');
-});
+// Set up the app
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // Create server
-app.listen(port, () => {
-  console.log(`The application is running on localhost:${port}`);
+http.createServer(app).listen(3000);
+console.log('Server listening on port 3000');
+
+// Handle POST request
+app.post('/comment', function(req, res) {
+    var comment = req.body;
+    console.log('Adding comment: ' + JSON.stringify(comment));
+    comments.push(comment);
+    res.status(201).send(comment);
+});
+
+// Handle GET request
+app.get('/comment', function(req, res) {
+    res.status(200).send(comments);
+});
+
+// Handle DELETE request
+app.delete('/comment', function(req, res) {
+    comments = [];
+    res.status(204).send('All comments deleted');
 });
