35725e0f022680fcb5b253aa50eb5ca0516fe5e3
Taha Kaan Yılmaz <142171450+kaanylmz34@users.noreply.github.com>
2024-02-22 10:28:02 +0000

    step 4

--- comments.js
+++ comments.js
@@ -1,35 +1,17 @@
 // Create Web Server
+const http = require('http');
 
-// Required modules
-var http = require('http');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var comments = [];
-
-// Set up the app
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-// Create server
-http.createServer(app).listen(3000);
-console.log('Server listening on port 3000');
-
-// Handle POST request
-app.post('/comment', function(req, res) {
-    var comment = req.body;
-    console.log('Adding comment: ' + JSON.stringify(comment));
-    comments.push(comment);
-    res.status(201).send(comment);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
 });
 
-// Handle GET request
-app.get('/comment', function(req, res) {
-    res.status(200).send(comments);
-});
-
-// Handle DELETE request
-app.delete('/comment', function(req, res) {
-    comments = [];
-    res.status(204).send('All comments deleted');
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
